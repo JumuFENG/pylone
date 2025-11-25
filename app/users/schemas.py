bfeee320b4ca6377619262b5f9fbeb626ef28b6b
JumuFENG <zhcbfly@qq.com>
2025-11-25 16:33:17 +0800

    Initial the pylone project.

--- app/users/schemas.py
+++ app/users/schemas.py
@@ -4,9 +4,15 @@ from pydantic import Field
 
 class UserRead(schemas.BaseUser[int]):
     username: str
+    parent_id: Optional[int]
+    realcash: Optional[int]
 
 class UserCreate(schemas.BaseUserCreate):
     username: str
+    parent_id: Optional[int]
+    realcash: Optional[int] = 0
 
 class UserUpdate(schemas.BaseUserUpdate):
     username: Optional[str] = None
+    parent_id: Optional[int] = None
+    realcash: Optional[int] = 0
